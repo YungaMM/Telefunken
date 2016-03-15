9627ff4fc73e21aee1b9b0db8908b47ad2f3b6fb
Yungamm <yungamm@yandex.ru>
2016-03-15 17:54:23 +0300

    1

--- src/Loader.java
+++ src/Loader.java
@@ -1,6 +1,8 @@
 import org.javagram.TelegramApiBridge;
+import org.javagram.response.AuthAuthorization;
 import org.javagram.response.AuthCheckedPhone;
 import org.javagram.response.AuthSentCode;
+import org.javagram.response.object.User;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,14 +12,21 @@ public class Loader {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        TelegramApiBridge bridge = new TelegramApiBridge("149.154.167.50:443",36025,"5530d508831b42dc0df01f9b31c2978b");
+        TelegramApiBridge bridge = new TelegramApiBridge("149.154.167.50:443", 36025, "5530d508831b42dc0df01f9b31c2978b");
 
         System.out.println("Please, type phone number");
         String userPhone = reader.readLine().trim();
         AuthCheckedPhone checkedPhone = bridge.authCheckPhone(userPhone);
-        if(checkedPhone.isRegistered()){
-            AuthSentCode sentCode = bridge.authSendCode(userPhone);
+        if (checkedPhone.isRegistered()) {
+            bridge.authSendCode(userPhone);
 
+            System.out.println("Please, type SMS code");
+            String smsCode = reader.readLine().trim();
+            AuthAuthorization authorization = bridge.authSignIn(smsCode);
+            User user = authorization.getUser();
+            System.out.println("FirstName: " + user.getFirstName());
+            System.out.println("LastName: " + user.getLastName());
+            System.out.println("UserPhone: " + user.getPhone());
         }
 
     }
